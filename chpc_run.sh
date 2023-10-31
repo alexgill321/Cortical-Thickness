a8bc9d3beeff1d98b7cb7ba5d9d5c23b47086816
alexgill321 <alexgill321@gmail.com>
2023-10-30 19:53:13 -0600

    chpc changes

--- chpc_run.sh
+++ chpc_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account soc-gpu-np
 #SBATCH --partition soc-gpu-np
-#SBATCH --ntasks-per-node=32
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:a100:1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=3
+#SBATCH --gres=gpu:2080ti:3
 #SBATCH --time=8:00:00
-#SBATCH --mem=40GB
+#SBATCH --mem=2GB
 #SBATCH --mail-user=u1380656@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
 #SBATCH -o cv_large-%j
